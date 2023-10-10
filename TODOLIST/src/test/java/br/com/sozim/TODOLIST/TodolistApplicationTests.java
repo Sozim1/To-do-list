e497bbe320534cfdf49dd8fc0a14cabf4df77cee
Gabriel Alves <119307561+Sozim1@users.noreply.github.com>
2023-10-10 19:03:18 -0300

    Add files via upload

---
+++ TODOLIST/src/test/java/br/com/sozim/TODOLIST/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sozim.TODOLIST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
